3d88fabcccf77d82f24182aea5e5119e65f8e312
RaKalik963 <andrejmahot646@gmail.com>
2024-06-26 19:09:42 +0300

    lab-ex2

--- main.cpp
+++ main.cpp
@@ -5,8 +5,79 @@ using namespace std;
 
 
 
-int main()
-{
+template<typename T>
+T** createMatrix(int row, int col) {
+    T** arr = new T * [row];
+    for (int i = 0; i < row; i++) {
+        arr[i] = new T[col];
+    }
+    return arr;
+}
+
+template<typename T>
+void fillMatrix(T** arr, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            arr[i][j] = rand() % 10;
+        }
+    }
+}
+
+template<typename T>
+void printMatrix(T** arr, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T>
+T** addRowToStart(T** arr, int& row, int col, T* newRow) {
+    T** newArr = createMatrix<T>(row + 1, col);
+    for (int j = 0; j < col; j++) {
+        newArr[0][j] = newRow[j];
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            newArr[i + 1][j] = arr[i][j];
+        }
+    }
+    row++;
+    return newArr;
+}
+
+int main() {
+    srand(time(0));
+    int col, row;
+    cout << "enter column: ";
+    cin >> col;
+    cout << "enter rows: ";
+    cin >> row;
+
+    int** arr = createMatrix<int>(row, col);
+    fillMatrix(arr, row, col);
+
+    cout << "Original array:" << endl;
+    printMatrix(arr, row, col);
+
+    int* newRow = new int[col];
+    for (int i = 0; i < col; i++) {
+        newRow[i] = rand() % 10;
+    }
+
+    int** newArr = addRowToStart(arr, row, col, newRow);
+
+    cout << "Array after adding a new row to the start:" << endl;
+    printMatrix(newArr, row, col);
+
+    for (int i = 0; i < row; i++) {
+        delete[] newArr[i];
+    }
+    delete[] newArr;
+    delete[] newRow;
+
 	system("pause");
 	return 0;
 }
